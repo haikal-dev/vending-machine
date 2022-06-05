dc24a71e9c042dd80055a4b787c6d79aba8ab436
mhaikalazizan <mhaikalazizan@gmail.com>
2022-06-05 17:40:36 +0800

    select item function with balance

--- main.cpp
+++ main.cpp
@@ -1,6 +1,31 @@
 #include <iostream>
 using namespace std;
 
+int selectItem(int coin){
+    int item;
+    int price[5] = {2,1,1,3,2};
+
+    cout << "Please select the items (1, 2, 3, 4, 5):\n";
+    cin >> item;
+
+    if(item > 5 || item < 1){
+        cout << "Opps! You have entered wrong item!";
+    }
+
+    else {
+        cout << "You have entered item " << item << " with price RM" << price[item-1] << "\n";
+
+        // calculate to get balance
+        int balance = 0;
+
+        balance = coin - price[item-1];
+
+        cout << "Your balance is RM" << balance << "\n";
+    }
+
+    return 0;
+}
+
 int main() {
 
     int coin;
@@ -9,7 +34,7 @@ int main() {
   cin >> coin;
 
   if(coin == 1 || coin == 5 || coin == 10){
-    cout << "OK! Please select the items:\n";
+    selectItem(coin);
   }
 
   else {
@@ -18,6 +43,3 @@ int main() {
 
   return 0;
 }
-
-int aisyah()
-{
